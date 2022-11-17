ec737adba6daa4f3dbcfe714dc0d295874743246
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2022-11-17 08:09:43 +0100

    files updated

---
+++ src/components/ErrorMsg/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+interface ErrorMsgProps {
+  content: string;
+}
+
+const ErrorMsg: FC<ErrorMsgProps> = ({
+  content,
+}) => {
+  return (
+    <div className='alert-danger'>{content}</div>
+  );
+};
--- src/pages/dashboard/update-product/index.tsx
+++ src/pages/dashboard/update-product/index.tsx
@@ -1,9 +1,7 @@
-function UpdateProductPage() {
-  return (
-    <div>
-      UpadateProductPage
-    </div>
-  )
+import { NextPage } from 'next';
+
+const UpdateProductPage: NextPage = () => {
+  return <div>UpadateProductPage</div>;
 };
 
 export default UpdateProductPage;
